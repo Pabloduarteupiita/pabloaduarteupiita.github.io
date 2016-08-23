5220bbaabf56590aec8ddeccaa74362b795cec88
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-23 17:13:04 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -20,6 +20,7 @@ forma.computeFaceNormals();
 
 var material= new THREE.MeshNormalMaterial();
 var malla= new THREE.Mesh(forma,material);
+
 malla.rotateX(Math.PI/4);
 
 var escena= new THREE.Scene();
