1dad51e6de0be3f1bae18f4fbf3fcfe9e3a4a6dc
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-23 12:26:28 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -11,5 +11,5 @@ var forma = new THREE.CircleGeometry( 1, 64 );
 var material = new THREE.MeshNormalMaterial(); 
 var circulo = new THREE.Mesh(forma, material);
 escena.add(circulo);
-circulo.position(10,5); 
+circulo.positionX(10); 
 renderizador.render(escena,camara);
