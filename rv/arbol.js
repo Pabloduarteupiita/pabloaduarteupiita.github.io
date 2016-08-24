4b45b4920c2bc03e77cd9a27e6d6724aca1f3b72
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-24 17:02:39 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -12,4 +12,9 @@ var material = new THREE.MeshNormalMaterial();
 var circulo = new THREE.Mesh(forma, material);
 escena.add(circulo);
 
+var geometry = new THREE.BoxGeometry( 1, 3 );
+var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
 renderizador.render(escena,camara);
