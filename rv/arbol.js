ba80dac5612d017d7cce5c4eb2b368959881c7d9
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-24 17:31:22 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -8,11 +8,11 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild (renderizador.domElement);
 
-var forma1 = new THREE.BoxGeometry( 1, 2);
+var forma1 = new THREE.BoxGeometry( 1, 3);
 var material1 = new THREE.MeshNormalMaterial();
 var cube = new THREE.Mesh( forma1, material1 );
+cube.position.y += -1;
 escena.add(cube);
-
 var forma = new THREE.CircleGeometry( 1, 64 );
 var material = new THREE.MeshNormalMaterial(); 
 var circulo = new THREE.Mesh(forma, material);
