df4271cd37751c796eb1af5b897988efbef8bdeb
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-24 17:13:10 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -5,8 +5,7 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-                    	window.innerHeight*.95);
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild (renderizador.domElement);
 
 var forma = new THREE.CircleGeometry( 1, 64 );
