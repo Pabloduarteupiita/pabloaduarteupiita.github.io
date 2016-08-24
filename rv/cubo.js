f246d520621f7908d625cf7037ec0cf2ee068e67
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-24 17:54:50 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -1,19 +1,19 @@
 var forma= new THREE.Geometry();
-forma.vertices.push(new THREE.Vector3(1,1,1));
-forma.vertices.push(new THREE.Vector3(1,1,-1));
-forma.vertices.push(new THREE.Vector3(1,-1,1));
-forma.vertices.push(new THREE.Vector3(1,-1,-1));
-forma.vertices.push(new THREE.Vector3(-1,1,1));
-forma.vertices.push(new THREE.Vector3(-1,1,-1));
-forma.vertices.push(new THREE.Vector3(-1,-1,1));
-forma.vertices.push(new THREE.Vector3(-1,-1,-1));
+forma.vertices.push(new THREE.Vector3(1,1,1));//0
+forma.vertices.push(new THREE.Vector3(1,1,-1));//1
+forma.vertices.push(new THREE.Vector3(1,-1,1));//2
+forma.vertices.push(new THREE.Vector3(1,-1,-1));//3
+forma.vertices.push(new THREE.Vector3(-1,1,1));//4
+forma.vertices.push(new THREE.Vector3(-1,1,-1));//5
+forma.vertices.push(new THREE.Vector3(-1,-1,1));//6
+forma.vertices.push(new THREE.Vector3(-1,-1,-1));7
 
 forma.faces.push(new THREE.Face3(1,2,3)); //cara 0
-forma.faces.push(new THREE.Face3(1,2,5)); //cara 1
-forma.faces.push(new THREE.Face3(1,3,7)); //cara 2
-forma.faces.push(new THREE.Face3(3,4,8)); //cara 3
-forma.faces.push(new THREE.Face3(7,5,6)); //cara 4
-forma.faces.push(new THREE.Face3(4,8,6)); //cara 5
+//forma.faces.push(new THREE.Face3(1,2,5)); //cara 1
+//forma.faces.push(new THREE.Face3(1,3,7)); //cara 2
+//forma.faces.push(new THREE.Face3(3,4,8)); //cara 3
+//forma.faces.push(new THREE.Face3(7,5,6)); //cara 4
+//forma.faces.push(new THREE.Face3(4,8,6)); //cara 5
 
 
 forma.computeBoundingSphere();
