2f24ee1d26c51a267f756163ed152bbc0631a4bd
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-24 18:50:47 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -17,9 +17,9 @@ forma.faces.push(new THREE.Face3(2,6,7)); //cara 5
 forma.faces.push(new THREE.Face3(2,3,7)); //cara 6
 forma.faces.push(new THREE.Face3(2,5,7)); //cara 7
 forma.faces.push(new THREE.Face3(3,2,6)); //cara 8
-//forma.faces.push(new THREE.Face3(,,)); //cara 9
-//forma.faces.push(new THREE.Face3(,,)); //cara 10
-//forma.faces.push(new THREE.Face3(,,)); //cara 11
+forma.faces.push(new THREE.Face3(3,6,4)); //cara 9
+forma.faces.push(new THREE.Face3(,,)); //cara 10
+forma.faces.push(new THREE.Face3(,,)); //cara 11
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
