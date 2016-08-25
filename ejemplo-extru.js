e6111f596c731065eceaac0b9dc261a82b5e6e63
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-25 15:55:49 -0500

    Update ejemplo-extru.js

--- ejemplo-extru.js
+++ ejemplo-extru.js
@@ -1,3 +1,8 @@
+var puntos=[];
+
+for(var i=0; i<50; i++){
+puntos.push(new THREE.Vector2(Math.sin(i*0.2)*15+50, (i-5)*2))};
+
 var material =new THREE.MeshNormalMaterial();
 
 var malla= new THREE.Mesh(forma,material);
@@ -5,10 +10,8 @@ malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(malla);
-
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
-
 var renderizador= new THREE.WebGLRender();
 renderizador.setSize{window.innerHeight*.95,window.innerHeight*.95};
 document.body.appendChild(renderizador.domElement);
