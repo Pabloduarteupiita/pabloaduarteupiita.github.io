b9a35d17fdc48d0395f780a32441858ab59d7abe
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-26 10:57:16 -0500

    Update ejemplo-extru.js

--- ejemplo-extru.js
+++ ejemplo-extru.js
@@ -13,6 +13,6 @@ escena.add(malla);
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
 var renderizador= new THREE.WebGLRender();
-renderizador.setSize{window.innerHeight*.95,window.innerHeight*.95};
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
