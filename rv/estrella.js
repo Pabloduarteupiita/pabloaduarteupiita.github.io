db80e61808926e4af395f18239e214bddfc31064
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-30 17:40:13 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -1,15 +1,15 @@
 var figura = new THREE.Shape(); //para figura es el constructor shape
 //figura.moveTo(10,0); //mover a coordenada 10x 10y
-figura.lineTo(15,15); // hacer linea en coordenadas 10x 40x 
-figura.lineTo(40,0);
-figura.lineTo(30,20);
-figura.lineTo(40,30);/////////
-figura.lineTo(20,30);
-figura.lineTo(20,40);
-figura.lineTo(5,30);
-figura.lineTo(0,30);
-figura.lineTo(10,20);
-figura.lineTo(0,0);
+figura.lineTo(20,15); // hacer linea en coordenadas 10x 40x 1
+figura.lineTo(40,0); //2
+figura.lineTo(30,20);//3
+figura.lineTo(40,30);/////////4
+figura.lineTo(30,30);//5
+figura.lineTo(20,40);//6
+figura.lineTo(5,30);//7
+figura.lineTo(0,30);//8
+figura.lineTo(10,20);//9
+figura.lineTo(0,0);//10
 
 var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.Mesh(forma);
