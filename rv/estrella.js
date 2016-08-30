90df3ab2f34be05cd017fc42bc9d043e2d9f9ca4
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-30 17:10:28 -0500

    Create estrella.js

---
+++ rv/estrella.js
@@ -0,0 +1,22 @@
+var figura = new THREE.Shape(); //para figura es el constructor shape
+//figura.moveTo(10,0); //mover a coordenada 10x 10y
+figura.lineTo(20,20); // hacer linea en coordenadas 10x 40x 
+figura.lineTo(15,55);
+figura.lineTo(10,70);
+figura.lineTo(15,70);
+figura.lineTo(20,90);
+figura.lineTo(10,0);
+
+var forma = new THREE.ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=100;
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
