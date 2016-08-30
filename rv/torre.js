43c12f722d1665a8aae216a910e4e4cd1c02b091
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-30 15:34:24 -0500

    Create torre.js

---
+++ rv/torre.js
@@ -0,0 +1,26 @@
+var torreForma= new THREE.CylinderGeometry(.5,.5,1);
+var torreForma1= new THREE.CylinderGeometry(.5,.5,1);
+torreForma1.translate(0,1,0);
+
+var torreMalla= new THREE.Mesh(torreForma);
+var torreMalla1= new THREE.Mesh(torreForma1);
+
+var torreForma= new THREE.Geometry();
+
+torreForma.merge(torreMalla1.geometry,torreMalla.matrix);
+torreForma.merge(torreMalla.geometry,torreMalla1.matrix);
+
+var material= new THREE.MeshNormalMaterial();
+var toreMalla= new THREE.Mesh(torreForma,material);
+
+var escena= new THREE.Scene();
+escena.add(torreMalla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara)
