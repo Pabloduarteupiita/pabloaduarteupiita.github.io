4f36de39fdc057c8c69cbc9f713733bec4904e5f
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-08-30 16:30:32 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -2,8 +2,8 @@ var p1Forma= new THREE.CylinderGeometry(.5,.5,1);
 var p2Forma= new THREE.CylinderGeometry(.7,.7,.5);
 var p3Forma= new THREE.CylinderGeometry(.7,.7,.5);
 
-p2Forma.translate(0,3,3);
-p2Forma.translate(0,-3,3);
+p2Forma.translate(3,3,3);
+p2Forma.translate(3,-3,3);
 
 var p1Malla= new THREE.Mesh(p1Forma);
 var p2Malla= new THREE.Mesh(p2Forma);
