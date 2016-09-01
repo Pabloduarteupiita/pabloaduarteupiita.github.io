3b12d98f2b9af5aae0d565f9752b66bc3aaf6111
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-01 15:06:12 -0500

    Update picos.js

--- picos.js
+++ picos.js
@@ -1,9 +1,15 @@
 var figura= new THREE.Shape();
 
-figura.moveTo(10,10);
-figura.lineTo(10,40);
-figura.lineTo(40,40);
-figura.lineTo(10,10);
+figura1.moveTo(10,10);
+figura1.lineTo(30,10);
+figura1.lineTo(30,30);
+figura1.lineTo(10,10);
+figura1.lineTo(10,10);
+
+
+
+
+
 
 var forma = new THREE.ExtrudeGeometry(figura,{amount:10});
 
