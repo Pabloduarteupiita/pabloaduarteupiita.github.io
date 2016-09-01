783eba07de810fd0abbfcb8ef87b636890b450da
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-01 14:58:53 -0500

    Update ejemplo-extru.js

--- ejemplo-extru.js
+++ ejemplo-extru.js
@@ -8,7 +8,7 @@ figura.lineTo(10,10);
 var forma = new THREE.ExtrudeGeometry(figura,{amount:10});
 
 var forma= new THREE.MeshNormalMaterial();
-var malla= new THREE.Mesh(forma,meterial);
+var malla= new THREE.Mesh(forma,material);
 malla.rotateX(Math.PI/4)
 
 var escena = new THREE.Scene();
