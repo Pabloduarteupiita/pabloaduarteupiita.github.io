f6dbf00e798d214786594713be929cb605b3b49e
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-06 15:19:46 -0500

    Update ejemplo-camaraorto.js

--- ejemplo-camaraorto.js
+++ ejemplo-camaraorto.js
@@ -6,7 +6,7 @@ camara.bottom=window.innerHeight/-2;
 camara.near=0.1;
 camara.far=1000;
 camara.updateProjectionMatrix();
-camara.position.za=100;
+camara.position.z=100;
 
 var cubo=new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math,PI/4);
