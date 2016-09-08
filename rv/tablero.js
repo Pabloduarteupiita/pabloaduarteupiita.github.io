d97c28ccefc642ba7b63293773af22ad63ca2b6a
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-08 15:02:08 -0500

    Create tablero.js

---
+++ rv/tablero.js
@@ -0,0 +1,23 @@
+var cubo1Forma= new THREE.BoxGeometry(1,1,1);
+cuboForma.translate(1,0,0);
+
+var tableroMalla= new THREE.Mesh(cubo1Forma);
+
+var tableroForma= new THREE.Geometry();
+tableroForma.merge(tableroMalla.geometry,tableroMalla.matrix);
+
+
+var material= new THREE.MeshNormalMaterial();
+var tableroMalla= new THREE.Mesh(tableroForma,material);
+
+var escena= new THREE.Scene();
+escena.add(tableroMalla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara)
