52bcb7d080398ebf8ffe95466db7fa1a47b5429b
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-08 15:29:28 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -9,18 +9,13 @@ var cube2 = new THREE.Mesh( cubo2Forma, material1 );
 cubo1Forma.translate(0,0,0);
 cubo2Forma.translate(1,0,0);
 
-
 var escena= new THREE.Scene();
-escena.add(tableroMalla);
+escena.add(cube1);
+escena.add(cube2);
 
 var camara= new THREE.PerspectiveCamera();
-
-
 camara.position.z=5;
-
-
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
-
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara)
