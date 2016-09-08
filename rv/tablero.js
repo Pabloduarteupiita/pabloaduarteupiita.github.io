f3b65c6576dd61b3d6f975d544b0acf52a20d829
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-08 15:35:12 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -2,7 +2,7 @@ var cubo1Forma= new THREE.BoxGeometry(1,1,1);
 var cubo2Forma= new THREE.BoxGeometry(1,1,1);
 
 var material1 = new THREE.MeshBasicMaterial({color: 0xffffff});
-var material2 = new THREE.MeshBasicMaterial({color: 0x996600});
+var material2 = new THREE.MeshBasicMaterial({color: 0x808080});
 
 var cube1 = new THREE.Mesh( cubo1Forma, material1 );
 var cube2 = new THREE.Mesh( cubo2Forma, material2 );
