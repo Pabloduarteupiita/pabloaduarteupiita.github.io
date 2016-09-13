2748cd2faeee362c66153fece3a257514cd5eaa1
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-13 15:08:10 -0500

    Create ejemplo-meshlambertmaterial.js

---
+++ ejemplo-meshlambertmaterial.js
@@ -0,0 +1,21 @@
+var luzPuntual= new THREE.pointLight(0xFFFFFFF);
+luzPuntual.position.x=10;
+luzPuntual.position.y=10;
+luzPuntual.position.z=10;
+
+var forma=    new THREE.SphereGeometry(1);
+var material= new THREE.MeshLambertMaterial({color:'#00CC00'});
+var malla=    new THREE.Mesh(forma,material);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+escena.add(luzPuntual);
+
+var camara=new THREE.PespectiveCamera();
+camara.position.z=5;
+
+var lienzo=document.getElementaryId("luzPuntual");
+var renderizador= new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
+
+renderizador.setsize(600,600);
+renderizador.render(escena,camara);
