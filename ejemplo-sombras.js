53e77e948f06fdd9cb2fe6abdf5e011090e15794
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-14 17:43:47 -0500

    Update ejemplo-sombras.js

--- ejemplo-sombras.js
+++ ejemplo-sombras.js
@@ -5,7 +5,7 @@ var forma=     new THREE.SphereGeometry(1);
 var material=  new THREE.MeshLambertMaterial({color:"#00cc00"});
 var malla=     new THREE.Mesh(forma,material);
 
-var base= new THREE.Mesh(new THREE.BoxGeometry(5,.1,5),new THREE.MeshLambertMaterial({color: "0xFFFFFF"}));
+var base= new THREE.Mesh(new THREE.BoxGeometry(5,.1,5),new THREE.MeshLambertMaterial({color:0xFFFFFF}));
 
 var escena= new THREE.Scene();
 escena.add(malla);
