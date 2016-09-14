f2166258caa9e90a2b3384d3b34198b1b38a5225
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-14 18:46:45 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -6,15 +6,15 @@ var geometry = new THREE.Geometry();
 
 				for ( var i = - size; i <= size; i += step ) {
 
-					geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-					geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+					geometry.vertices.push( new THREE.BoxGeometry(1,1,1)( - size, 0, i ) );
+					geometry.vertices.push( new THREE.BoxGeometry(1,1,1)(   size, 0, i ) );
 
-					geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-					geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+					geometry.vertices.push( new THREE.BoxGeometry(1,1,1)( i, 0, - size ) );
+					geometry.vertices.push( new THREE.BoxGeometry(1,1,1)( i, 0,   size ) );
 
 				}
 
-var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+var material = new THREE.LineBasicMaterial( { color: 0x00000F} );
 var line = new THREE.LineSegments( geometry, material );
 var scene= new THREE.Scene();
 scene.add( line );
