6fdd5970156a9dc0591d99a6054b6308fc010cb3
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-18 13:59:58 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -290,14 +290,57 @@ var material2 = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
 var grisesmalla= new THREE.Mesh(grises,material1);
 var blancosmalla= new THREE.Mesh(blancos,material2);
 
+///////////////////////////////////TORRE
 
+var p1Forma= new THREE.CylinderGeometry(.7,.7,1);
+var p2Forma= new THREE.CylinderGeometry(1,1,.3);
+var p3Forma= new THREE.CylinderGeometry(.8,1,.6);
+
+var p4Forma= new THREE.CylinderGeometry(0,.2,.2);
+var p5Forma= new THREE.CylinderGeometry(0,.2,.2);
+var p6Forma= new THREE.CylinderGeometry(0,.2,.2);
+var p7Forma= new THREE.CylinderGeometry(0,.2,.2);
+
+p2Forma.translate(1,.5,1);
+p3Forma.translate(1,-.5,1);
+
+p4Forma.translate(.5,1,1.5);
+p5Forma.translate(.5,1,1.5);
+p6Forma.translate(-.5,1,1.5);
+p7Forma.translate(-.2,1,.8);
+
+
+var p1Malla= new THREE.Mesh(p1Forma);
+var p2Malla= new THREE.Mesh(p2Forma);
+var p3Malla= new THREE.Mesh(p3Forma);
+
+var p4Malla= new THREE.Mesh(p4Forma);
+var p5Malla= new THREE.Mesh(p5Forma);
+var p6Malla= new THREE.Mesh(p6Forma);
+var p7Malla= new THREE.Mesh(p7Forma);
+
+var torreForma= new THREE.Geometry();
+
+torreForma.merge(p1Malla.geometry,p1Malla.matrix);
+torreForma.merge(p2Malla.geometry,p2Malla.matrix);
+torreForma.merge(p3Malla.geometry,p3Malla.matrix);
+
+torreForma.merge(p4Malla.geometry,p4Malla.matrix);
+torreForma.merge(p5Malla.geometry,p5Malla.matrix);
+torreForma.merge(p6Malla.geometry,p6Malla.matrix);
+torreForma.merge(p7Malla.geometry,p7Malla.matrix);
+
+var material= new THREE.MeshNormalMaterial();
+var torreMalla= new THREE.Mesh(torreForma,material);
+//////////////////////////////
 var escena= new THREE.Scene();
 escena.add(blancosmalla);
 escena.add(grisesmalla);
-
+escena.add(torreMalla);
+////////////////////////////////////CAMARA
 var camara= new THREE.PerspectiveCamera();
 camara.position.z=20;
-
+/////////////////////////////
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
