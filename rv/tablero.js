269d5e48abb844dc1797857d0ceddb7b6f4e7964
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-18 14:57:21 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -346,9 +346,7 @@ var relacionAspecto=window.innerWidth/window.innerHeight;
 var planoCercano=1;
 var planoLejano=1000;
 var camara= new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano)
-camara.position.z=15;
-
-camara.rotateZ(Math.PI/2);
+camara.position.z=8;
 /////////////////////////////
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
