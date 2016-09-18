0ac286a3dfd5f82fe09c05b5fb6f8c9a1207d32e
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-18 12:53:20 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -270,8 +270,9 @@ escena.add(grisesmalla);
 
 
 var camara= new THREE.PerspectiveCamera();
-camara.position.z=5;
-				
+camara.position.z=10;
+camara.position.x=5;
+camara.position.y=5;
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
