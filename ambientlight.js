80e7a0636b64469aaeb12f95ff8672afefaade6a
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-20 15:07:53 -0500

    Update ambientlight.js

--- ambientlight.js
+++ ambientlight.js
@@ -12,7 +12,7 @@ var camara=   new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var lienzo= document.getElementById("LuzAmbiental");
-var renderizador= new THREE.WebGLRenderer({canvas});
+var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
