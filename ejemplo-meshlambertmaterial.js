eab8e98c858bdce870812d1c7010b560ca57e3b4
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-20 15:22:40 -0500

    Update ejemplo-meshlambertmaterial.js

--- ejemplo-meshlambertmaterial.js
+++ ejemplo-meshlambertmaterial.js
@@ -1,23 +1,22 @@
-var luzPuntual= new THREE.PointLight(0xFFFFFFF);
-luzPuntual.position.x=10;
-luzPuntual.position.y=10;
-luzPuntual.position.z=10;
+var luzPuntual = new THREE.PointLight(0xFFFFFF);
+luzPuntual.position.x = 10;
+luzPuntual.position.y = 10;
+luzPuntual.position.z = 10;
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial( { color: '#00cc00' } );;
+//Las caras se ven afectadas por la iluminación
+//Son superficies de tipo mate, llamadas superficies de Lambert
+var malla = new THREE.Mesh( forma, material );
 
-var forma=    new THREE.SphereGeometry(1);
-var material= new THREE.MeshLambertMaterial({color:'#00cc00'});
-var malla=    new THREE.Mesh(forma,material);
-
-var escena= new THREE.Scene();
+var escena = new THREE.Scene();
 escena.add(malla);
 escena.add(luzPuntual);
 
-var camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
 
-var lienzo=document.getElementById("luzPuntual");
-//var renderizador= new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
-var renderizador= new THREE.WebGLRenderer();
-renderizador.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderizador.domElement);
-renderizador.setsize(600,600);
-renderizador.render(escena,camara);
+var lienzo = document.getElementById("lambertMaterial");
+var renderizador = new THREE.WebGLRenderer( { canvas: lienzo, antialias: true } );
+
+renderizador.setSize( 600, 600 );
+renderizador.render( escena, camara );
