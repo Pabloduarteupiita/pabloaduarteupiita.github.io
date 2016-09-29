980f972678d7ec03b6352a62a221225f5fa44201
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-09-29 15:52:13 -0500

    Update texturas.js

--- texturas.js
+++ texturas.js
@@ -11,17 +11,14 @@ cargador.load("earth.jpg",TEXTURA.retrollamada);
 TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
 TEXTURA.camara.position.z=5; 
 var lienzo=document.getElementById("ejemplo-ventana"); 
-//var renderizador= new THREE.WebGLRenderer();
 TEXTURA.renderizador= new THREE.WebGLRenderer({canvas: lienzo, antialias: true}); 
 TEXTURA.renderizador.setSize(600, 600); 
-//document.body.appendChild(renderizador.domElement);
 } 
-
 TEXTURA.loop = function() { 
 requestAnimationFrame(TEXTURA.loop); 
 if (TEXTURA.malla != undefined){ 
-     TEXTURA.malla.rotateX(0.01); 
-     TEXTURA.malla.rotateY(0.01); 
+    TEXTURA.malla.rotateX(0.01); 
+    TEXTURA.malla.rotateY(0.01); 
 } 
   TEXTURA.renderizador.render(TEXTURA.escena,TEXTURA.camara); 
 } 
