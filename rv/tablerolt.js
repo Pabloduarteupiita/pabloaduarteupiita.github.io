4dd8445160585cd7843469db1d8ff40901d843bb
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-10-16 15:51:13 -0500

    Update tablerolt.js

--- rv/tablerolt.js
+++ rv/tablerolt.js
@@ -346,6 +346,8 @@ camara.position.z=10;
 camara.position.y=10;
 camara.position.x=10;
 torreMalla.rotateZ(Math.PI/2);
+torreMalla.rotateX(Math.PI/2);
+torreMalla.rotateY(Math.PI/2);
 /////////////////////////////
 ///luces de colores 
 var luzPuntual1 = new THREE.PointLight(0xFFFFFF);
