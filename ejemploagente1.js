f8a0af48e3721621fefab466205fb74604148d8a
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-10-25 15:46:03 -0500

    Update ejemploagente1.js

--- ejemploagente1.js
+++ ejemploagente1.js
@@ -1,3 +1,52 @@
+function Agent(x=0,y=0){
+THREE.Object3D.call(this);
+this.position.x=x;
+this.position.y=y;   
+}
+
+Agent.Prototype=new THREE.Object3D();
+Agent.prototype.sense= function(enviroment){};
+Agent.prototype.plan= function(enviroment){};
+Agent.prototype.act= function(enviroment){};
+
+function Enviroment(){
+THREE.Scene.call(this);
+
+Enviroment.prototype=new THREE.Scene();
+Enviroment.prototype.sense=function(){
+for (var i=0; i<this.children.lenght;i++){
+if(this.children[i].sense!==undefined)
+hhis.chlidren[i].sense(this);
+}
+}
+//interfaz entre entorno y agente
+Enviroment.prototype.plan=function(){
+for(var i=0; i<this.children.length;i++){
+if (this.children[i].plan !== undefined)
+this.children[i].plan(this);
+}
+}
+Enviroment.prototype.sense=function(){
+for(var i=0; i<this.children.length;i++){
+if (this.children[i].sense !== undefined)
+this.children[i].sense(this);
+}
+}
+Enviroment.prototype.act=function(){
+for(var i=0; i<this.children.length;i++){
+if (this.children[i].act !== undefined)
+this.children[i].act(this);
+}
+}
+
+
+
+
+
+
+
+
+
 function Pelota(r,x=0,y=0){
 Agent.call(this,x,y)
 this.add(new THREE.Mesh(new THREE.SphereGEometry(r),new THREE.MeshNormalMaterial()));
