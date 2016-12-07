db7d2cad518ac4e8d96694ed357b93f945d000a1
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-12-07 00:53:36 -0600

    Update tablerotexconpiezas.js

--- rv/tablerotexconpiezas.js
+++ rv/tablerotexconpiezas.js
@@ -87,20 +87,20 @@ var alfilMalla4= new THREE.Mesh(alfilForma,negras);
   
 alfilMalla1.rotateX(Math.PI/2);///ya
 alfilMalla1.translateY(3);
-alfilMalla1.translateZ(-70); 
-alfilMalla1.translateX(-50); 
+alfilMalla1.translateZ(-50); 
+alfilMalla1.translateX(-70); 
  
 alfilMalla2.rotateX(Math.PI/2);
 alfilMalla2.translateY(3);
-alfilMalla2.translateZ(-70);
-alfilMalla2.translateX(-20);
+alfilMalla2.translateZ(-20);
+alfilMalla2.translateX(-70);
  
-alfilMalla3.rotateX(Math.PI/2);
+alfilMalla3.rotateX(Math.PI/2);///ya
 alfilMalla3.translateY(3);
 alfilMalla3.translateZ(-50); 
 alfilMalla3.translateX(70);
  
-alfilMalla4.rotateX(Math.PI/2);
+alfilMalla4.rotateX(Math.PI/2);//ya
 alfilMalla4.translateY(3);
 alfilMalla4.translateZ(-20);
 alfilMalla4.translateX(70);
