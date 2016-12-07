33a37b52a2499fc811c2fcd80a64b8f83617d8b8
Pabloduarteupiita <pcastellanos6im04@gmail.com>
2016-12-07 00:41:45 -0600

    Update tablerotexconpiezas.js

--- rv/tablerotexconpiezas.js
+++ rv/tablerotexconpiezas.js
@@ -92,17 +92,17 @@ alfilMalla1.translateZ(-50);
 alfilMalla2.rotateX(Math.PI/2);
 alfilMalla2.translateY(3);
 alfilMalla2.translateZ(-20);
-
+alfilMalla4.translateX(20);
  
 alfilMalla3.rotateX(Math.PI/2);
 alfilMalla3.translateY(3);
 alfilMalla3.translateZ(-50); 
-alfilMalla4.translateX(70);
+
  
 alfilMalla4.rotateX(Math.PI/2);
 alfilMalla4.translateY(3);
 alfilMalla2.translateZ(20);
-alfilMalla4.translateX(70);
+
  
  
 /////////////////////////////////////////////Peon
